bc54af02ef6600bbc2de940225b5b8491806d9ca
A. Klingenberg <55751436+A-Klingenberg@users.noreply.github.com>
2023-11-11 23:34:47 +0100

    Add missing 'str' to isinstance()

--- ConfigManager.py
+++ ConfigManager.py
@@ -154,11 +154,11 @@ class ConfigManager(metaclass=_Singleton):
         if setting == "results_path":
             return self.results_path
         if self.command_args and setting in self.command_args and self.command_args[setting] is not None:
-            if isinstance(self.command_args[setting]) and len(self.command_args[setting]) == 0:
+            if isinstance(self.command_args[setting], str) and len(self.command_args[setting]) == 0:
                 return None
             return self.command_args[setting]
         elif self.file_config and setting in self.file_config and self.file_config[setting] is not None:
-            if isinstance(self.file_config[setting]) and len(self.file_config[setting]) == 0:
+            if isinstance(self.file_config[setting], str) and len(self.file_config[setting]) == 0:
                 return None
             return self.file_config[setting]
         else:
